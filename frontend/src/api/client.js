3aad1b13c38c8ad2bbe12d74039b228f96fe30d9
ahmed elrashid <ahmedpro787@gmail.com>
2026-05-04 21:10:05 +0100

    Change API_BASE

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = "http://wktime-backend-alb-915096209.eu-north-1.elb.amazonaws.com/api";
+export const API_BASE = import.meta.env.VITE_API_URL;
 
 const client = axios.create({
   baseURL: API_BASE
@@ -8,11 +8,12 @@ const client = axios.create({
 
 client.interceptors.request.use((config) => {
   const token = localStorage.getItem("wk_token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
 export default client;
-
